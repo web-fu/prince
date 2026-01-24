26d8ffb37c521189d9c1209d86cf6ccc27a540c4
web-fu <info@web-fu.it>
2026-01-24 19:15:19 +0100

    Hex grid parametric size

--- scripts/generation/WorldGenerator.gd
+++ scripts/generation/WorldGenerator.gd
@@ -1,16 +1,10 @@
 class_name WorldGenerator
 
 var rng := RandomNumberGenerator.new()
-var grid := HexGrid.new()
+var grid := HexGrid.new(Common.grid_size.cols, Common.grid_size.rows)
 
 func generate(seed:int):
 	rng.seed = seed
-
-	# crea griglia
-	for col in range(0, Common.grid_size.cols):
-		for row in range(0, Common.grid_size.rows):
-			grid.add_hex(col, row)
-
 	var plates = PlateGenerator.generate(grid, rng)
 	Tectonics.compute_stress(grid, plates)
 	ElevationGenerator.apply(grid)
