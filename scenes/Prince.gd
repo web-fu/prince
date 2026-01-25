781332cd420a679b39537fae7496b7a9fa7ee689
web-fu <info@web-fu.it>
2026-01-25 20:08:50 +0100

    TileSelected.move

--- scenes/Prince.gd
+++ scenes/Prince.gd
@@ -22,4 +22,4 @@ func _unhandled_input(event):
 		if offset:
 			var hex = grid.get_hex(offset.col, offset.row)
 			$TileInfo.show_tile(hex)
-			$WorldView/TileSelected.position = CoordConverter.offsetToWorld(hex.coord)
+			$WorldView/TileSelected.move(hex.coord)
