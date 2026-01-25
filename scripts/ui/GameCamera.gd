daa2abdada75fb386bd344c3d4a08f2f480d6d5e
web-fu <info@web-fu.it>
2026-01-25 19:08:30 +0100

    GameCamera update

--- scripts/ui/GameCamera.gd
+++ scripts/ui/GameCamera.gd
@@ -6,18 +6,19 @@ var direction = position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("camera_move_right"):
 		direction.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("camera_move_left"):
 		direction.x -= 1
-	if Input.is_action_pressed("move_down") and position.y > 5:
-		direction.y -=1
-	if Input.is_action_pressed("move_up") and position.y < 100 :
-		direction.y +=1
-	if Input.is_action_pressed("move_top") and position.z > 0:
-		direction.z -= 1
-	if Input.is_action_pressed("move_bottom") and position.z < Common.grid_size.rows:
+	if Input.is_action_pressed("camera_move_down") and position.z < Common.grid_size.rows * 2:
 		direction.z += 1
+	if Input.is_action_pressed("camera_move_up") and position.z > 0:
+		direction.z -= 1
+	if Input.is_action_pressed("camera_zoom_in") and position.y > 5:
+		direction.y -=1
+	if Input.is_action_pressed("camera_zoom_out") and position.y < 50:
+		direction.y +=1
+		
 	if position.x < - Common.grid_size.cols * Common.TILE_SIZE * sqrt(3) / 4:
 		position.x += Common.grid_size.cols * Common.TILE_SIZE * sqrt(3)
 		direction.x += Common.grid_size.cols * Common.TILE_SIZE * sqrt(3)
